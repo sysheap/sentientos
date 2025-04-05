fe7804af4917101df2174136f83e824ecfe00466
Maurice Hieronymus <mhi@mailbox.org>
2025-04-05 19:48:47 +0200

    Introduce nix shell

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+          rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain;
+          buildInputs = with pkgs; [
+            qemu
+            gdb
+            cargo-nextest
+            tmux
+          ];
+          nativeBuildInputs = with pkgs; [
+            rustToolchain
+            pkgsCross.riscv64-embedded.buildPackages.binutils
+            just
+          ];
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell {
+            inherit buildInputs nativeBuildInputs;
+          };
+        }
+      );
+}
