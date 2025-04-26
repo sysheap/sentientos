b86c3854a8e1b4c11bf4e108c54e02170ff15992
Maurice Hieronymus <mhi@mailbox.org>
2025-04-26 23:20:10 +0200

    Hello World from Posix
    Compile a small hello world binary in C and use ffi to provide a
    userspace libc.

---
+++ userspace/src/posix/hello_posix/hello_posix.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+  puts("Hello World from C");
+  return 0;
+}
---
+++ userspace/src/posix/include/stdio.h
@@ -0,0 +1,8 @@
+#ifndef _STDIO_H
+#define _STDIO_H	1
+
+#define EOF (-1)
+
+extern int puts (const char *__s);
+
+#endif
